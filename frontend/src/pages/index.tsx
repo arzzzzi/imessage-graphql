3aeff6a5f619d4a9fb078a2e9f405d3dbe3ca93f
arzzzzi <kantyshevmagomed@gmail.com>
2023-01-31 16:50:32 +0300

    connected mongodb

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next';
+import { signIn, useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  const { data: session } = useSession();
+  return (
+    <div>
+      <button onClick={() => signIn('google')}>Sign In</button>
+    </div>
+  );
 };
 
 export default Home;
